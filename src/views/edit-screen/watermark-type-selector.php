8c19f20a7b96f26f1ce7f73ab150d0d829ae3423
Wojtek Szałkiewicz <wojtek@underdev.it>
2019-01-07 19:32:20 +0100

    watermark type selector style

--- src/views/edit-screen/watermark-type-selector.php
+++ src/views/edit-screen/watermark-type-selector.php
@@ -1,5 +1,9 @@
 <div class="watermark-type-selector">
-	<p><?php _e( 'Watermark Type', 'easy-watermark' ); ?></p>
-	<label><input type="radio" name="watermark-type" id="watermark-type-image" value="image" /><?php _e( 'Image', 'easy-watermark' ); ?></label>
-	<label><input type="radio" name="watermark-type" id="watermark-type-text" value="text" /><?php _e( 'Text', 'easy-watermark' ); ?></label>
+	<h2><?php _e( 'Watermark Type', 'easy-watermark' ); ?></h2>
+	<div class="buttons">
+		<input type="radio" name="watermark-type" id="watermark-type-image" value="image" />
+		<label for="watermark-type-image" class="button first"><?php _e( 'Image', 'easy-watermark' ); ?></label>
+		<input type="radio" name="watermark-type" id="watermark-type-text" value="text" />
+		<label for="watermark-type-text" class="button last"><?php _e( 'Text', 'easy-watermark' ); ?></label>
+	</div>
 </div>
