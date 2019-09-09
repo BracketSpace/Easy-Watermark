21a2afb9e2510c9a39e0182ae6cf92103240c1c4
Wojtek Szałkiewicz <wojtek@szalkiewicz.pl>
2019-09-09 13:37:39 +0200

    Watermark delete link referer fix

--- src/views/edit-screen/metaboxes/watermark/submitdiv.php
+++ src/views/edit-screen/metaboxes/watermark/submitdiv.php
@@ -10,7 +10,12 @@
 	<div id="major-publishing-actions">
 		<div id="delete-action">
 		<?php	if ( current_user_can( 'delete_post', $post->ID ) ) : ?>
-			<a class="submitdelete deletion" href="<?php echo get_delete_post_link( $post->ID, '', true ); ?>"><?php esc_html_e( 'Delete Permanently' ); ?></a>
+			<?php
+			$delete_link = add_query_arg( [
+				'_wp_http_referer' => admin_url( 'tools.php?page=easy-watermark' ),
+			], get_delete_post_link( $post->ID, '', true ) );
+			?>
+			<a class="submitdelete deletion" href="<?php echo $delete_link; // phpcs:ignore ?>"><?php esc_html_e( 'Delete Permanently' ); ?></a>
 		<?php endif; ?>
 		</div>
 		<div id="publishing-action">
