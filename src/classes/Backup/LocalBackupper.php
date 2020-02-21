b429b25cd5a6eb774b6278a7da9e9e02a8b8bd78
Wojtek Szałkiewicz <wojtek@szalkiewicz.pl>
2020-02-21 13:46:18 +0100

    wp_normalize_path used for backup filepath

--- src/classes/Backup/LocalBackupper.php
+++ src/classes/Backup/LocalBackupper.php
@@ -59,7 +59,7 @@ class LocalBackupper implements BackupperInterface {
 			return new WP_Error( 'backup_error', __( 'Could not copy attachment file to backup directory.', 'easy-watermark' ) );
 		}
 
-		update_post_meta( $attachment_id, '_ew_backup_file', esc_sql( $new_filepath ) );
+		update_post_meta( $attachment_id, '_ew_backup_file', $new_filepath );
 
 		return true;
 
@@ -131,7 +131,7 @@ class LocalBackupper implements BackupperInterface {
 			return $backup_dir;
 		}
 
-		return $backup_dir . $filename;
+		return wp_normalize_path( $backup_dir . $filename );
 
 	}
 
