62d71950ed6e2fcb47bc85f1dabc26648a575d09
Wojtek Szałkiewicz <wojtek@szalkiewicz.pl>
2020-02-21 14:04:00 +0100

    Babel preset added

--- webpack.config.js
+++ webpack.config.js
@@ -43,6 +43,9 @@ module.exports = ( env, argv ) => {
 					exclude: /node_modules/,
 					use: {
 						loader: 'babel-loader',
+						options: {
+							presets: [ '@babel/preset-env' ],
+						},
 					},
 				},
 				{
