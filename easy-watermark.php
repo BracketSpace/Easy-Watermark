0ff47579a10aafa20de719b64d5234b64304e589
Jakub Mikita <jakub@bracketspace.com>
2021-02-05 16:59:30 +0100

    add github workflows

--- easy-watermark.php
+++ easy-watermark.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * Plugin Name:     Easy Watermark
- * Description:     This plugin can automatically add image and text watermark to pictures as they are uploaded to WordPress media library. You can also watermark existing images manually (all at once or an every single image). Watermark image can be a png, gif (alpha channel supported in both cases) or jpg. It's also possibile to set watermark opacity (doesn't apply to png with alpha channel). For text watermark you can select font, set color, size, angle and opacity.
- * Author:          BracketSpace
- * Author URI:      https://bracketspace.com/
- * Text Domain:     easy-watermark
- * Domain Path:     /languages
- * Version:         1.0.8
- * License:         GPLv3 or later
+ * Plugin Name: Easy Watermark
+ * Description: This plugin can automatically add image and text watermark to pictures as they are uploaded to WordPress media library. You can also watermark existing images manually (all at once or an every single image). Watermark image can be a png, gif (alpha channel supported in both cases) or jpg. It's also possibile to set watermark opacity (doesn't apply to png with alpha channel). For text watermark you can select font, set color, size, angle and opacity.
+ * Author: BracketSpace
+ * Author URI: https://bracketspace.com/
+ * Text Domain: easy-watermark
+ * Domain Path: /languages
+ * Version: 1.0.8
+ * License: GPLv3 or later
  *
- * @package         easy-watermark
+ * @package easy-watermark
  */
 
 define( 'EW_FILE_PATH', __FILE__ );
