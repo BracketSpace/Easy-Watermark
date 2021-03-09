ba1947e2e4935792d8611b485c8f391b71dcedff
Wojtek Szałkiewicz <wojtek@bracketspace.com>
2021-03-09 14:03:49 +0100

    add phpstan - CU-ek4r83

---
+++ src/inc/constants.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Constants definition for PHPStan
+ *
+ * @package easy-watermark
+ */
+
+define( 'EW_FILE_PATH', __FILE__ );
+define( 'EW_DIR_PATH', plugin_dir_path( __FILE__ ) );
+define( 'EW_DIR_URL', plugin_dir_url( __FILE__ ) );
