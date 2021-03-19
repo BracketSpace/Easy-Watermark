494dc9107ae8f15fe4f4be60e25e5372ca47a0cb
Wojtek Szałkiewicz <wojtek@bracketspace.com>
2021-03-19 16:34:53 +0100

    ad WatermarkPreview to metaboxes - CU-fezq8c

--- src/views/edit-screen/metaboxes/watermark/watermark-content.php
+++ src/views/edit-screen/metaboxes/watermark/watermark-content.php
@@ -5,6 +5,8 @@
  * @package easy-watermark
  */
 
+use EasyWatermark\Features\WatermarkPreview;
+
 global $post;
 
 ?>
@@ -45,7 +47,7 @@ global $post;
 	<div class="text-content">
 		<input class="watermark-text" name="watermark[text]" type="text" value="<?php echo esc_attr( $text ); ?>" placeholder="<?php esc_attr_e( 'Watermark text', 'easy-watermark' ); ?>" />
 		<p class="description"><?php esc_html_e( 'You can use placeholders listed in "Placeholders" metabox.', 'easy-watermark' ); ?></p>
-		<div class="text-preview" data-src="<?php echo esc_attr( site_url( sprintf( 'easy-watermark-preview/text-%s.png', $post->ID ) ) ); ?>"></div>
+		<div class="text-preview" data-src="<?php echo esc_url( WatermarkPreview::get_url( 'text', $post->ID ) ); ?>"></div>
 	</div>
 
 </div>
