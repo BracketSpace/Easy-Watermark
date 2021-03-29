209f3b49cbd0bd3fa4cde024bddc349c431f7fc2
Wojtek Szałkiewicz <wojtek@bracketspace.com>
2021-03-29 17:29:11 +0200

    add typescript - CU-gb8rre

--- webpack.config.js
+++ webpack.config.js
@@ -62,7 +62,7 @@ module.exports = ( env, argv ) => {
 					  ]
 					: [] ),
 				{
-					test: /\.jsx?$/,
+					test: /\.[jt]sx?$/,
 					exclude: /node_modules/,
 					use: {
 						loader: 'babel-loader',
