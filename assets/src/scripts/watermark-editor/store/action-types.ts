b75fd83cab653354bec6ef70a8b4d0eb5a11730e
Wojtek Szałkiewicz <wojtek@bracketspace.com>
2021-04-14 18:49:24 +0200

    add action type types - CU-gb8rre

---
+++ assets/src/scripts/watermark-editor/store/action-types.ts
@@ -0,0 +1,32 @@
+export const LOAD_WATERMARK_DATA: string = 'LOAD_WATERMARK_DATA';
+export const LOAD_WATERMARK_SUCCESS: string = 'LOAD_WATERMARK_SUCCESS';
+export const LOAD_WATERMARK_ERROR: string = 'LOAD_WATERMARK_ERROR';
+
+export const LOAD_EDITOR_SETTINGS: string = 'LOAD_EDITOR_SETTINGS';
+export const SAVE_EDITOR_SETTINGS: string = 'SAVE_EDITOR_SETTINGS';
+
+export const SET_IMAGE_SIZES: string = 'SET_IMAGE_SIZES';
+export const SET_IMAGE_TYPES: string = 'SET_IMAGE_TYPES';
+export const SET_POST_TYPES: string = 'SET_POST_TYPES';
+
+export const EDIT_WATERMARK_ATTRIBUTE: string = 'EDIT_WATERMARK_ATTRIBUTE';
+export const SET_WATERMARK_ATTRIBUTE: string = 'SET_WATERMARK_ATTRIBUTE';
+export const EDIT_WATERMARK_CONFIG: string = 'EDIT_WATERMARK_CONFIG';
+
+export const SAVE: string = 'SAVE';
+
+export const TOGGLE_FEATURE: string = 'TOGGLE_FEATURE';
+export const TOGGLE_SIDEBAR: string = 'TOGGLE_SIDEBAR';
+export const TOGGLE_SIDEBAR_TAB: string = 'TOGGLE_SIDEBAR_TAB';
+export const TOGGLE_SAVING: string = 'TOGGLE_SAVING';
+export const TOGGLE_PANEL: string = 'TOGGLE_PANEL';
+
+export const SET_EDITOR_STATE: string = 'SET_EDITOR_STATE';
+export const SET_EDITOR_POSITION: string = 'SET_EDITOR_POSITION';
+export const SET_EDITOR_SCALE: string = 'SET_EDITOR_SCALE';
+export const SET_EDITOR_PREVIEW_IMAGE: string = 'SET_EDITOR_PREVIEW_IMAGE';
+export const SET_EDITOR_POSITION_SCALE: string = 'SET_EDITOR_POSITION_SCALE';
+
+export const CREATE_OBJECT: string = 'CREATE_OBJECT';
+export const EDIT_OBJECT: string = 'EDIT_OBJECT';
+export const SET_ACTIVE_OBJECT: string = 'SET_ACTIVE_OBJECT';
