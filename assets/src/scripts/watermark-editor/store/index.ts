54dfb79be2c8ed82c89b3511a04e93518f6d7b8e
Wojtek Szałkiewicz <wojtek@bracketspace.com>
2021-04-14 18:48:34 +0200

    add store types - CU-gb8rre

--- assets/src/scripts/watermark-editor/store/index.ts
+++ assets/src/scripts/watermark-editor/store/index.ts
@@ -11,10 +11,11 @@ import * as actions from './actions';
 import * as selectors from './selectors';
 import applyMiddlewares from './middlewares';
 import { STORE_KEY, STORAGE_KEY } from './constants';
+import type { TStoreState, RemoveReturnTypes, OmitFirstArgs } from 'types';
 
 use( plugins.persistence, { storageKey: STORAGE_KEY } );
 
-const store = registerStore( STORE_KEY, {
+const store = registerStore<TStoreState>( STORE_KEY, {
 	reducer,
 	actions,
 	selectors,
@@ -22,5 +23,13 @@ const store = registerStore( STORE_KEY, {
 } );
 
 applyMiddlewares( store );
+
+export type TSelectors = OmitFirstArgs<typeof selectors>;
+export type TActions = RemoveReturnTypes<typeof actions>;
+
+declare module '@wordpress/data' {
+  function select( key: typeof STORE_KEY ): TSelectors;
+  function dispatch( key: typeof STORE_KEY ): TActions;
+}
 
 export default store;
